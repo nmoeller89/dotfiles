32905bcc40b8245bdf4c24076ac3cc5f22d135b6
nico moeller <13075503+nmoeller89@users.noreply.github.com>
2024-06-14 15:29:51 +0200

    fix typo

--- nvim/.config/nvim/lua/plugins/comments.lua
+++ nvim/.config/nvim/lua/plugins/comments.lua
@@ -3,7 +3,7 @@ return {
     "folke/todo-comments.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
     opts = {
-      vim.keymap.set("n", "<leader>tl", ":TodoQuickfix<CR>", { desc = "todo list" }),
+      vim.keymap.set("n", "<leader>tl", ":TodoQuickFix<CR>", { desc = "todo list" }),
       vim.keymap.set("n", "<leader>tf", ":TodoTelescope<CR>", { desc = "todo finder" })
     }
   }
