256991228481361a1684383ea680b9294bce70ac
nico moeller <13075503+nmoeller89@users.noreply.github.com>
2024-06-14 13:56:09 +0200

    add lazygit integration

--- nvim/.config/nvim/lua/plugins/git.lua
+++ nvim/.config/nvim/lua/plugins/git.lua
@@ -8,4 +8,13 @@ return {
       vim.keymap.set("n", "<leader>Gb", ":Gitsigns toggle_current_line_blame<CR>", { desc = "blame" })
     end
   },
+  {
+    "kdheepak/lazygit.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    config = function()
+      vim.keymap.set("n", "<leader>GG", ":LazyGit<CR>", { desc = "open lazygit" })
+    end
+  },
 }
